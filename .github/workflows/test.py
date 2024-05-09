8e861e514214f181f678d7b007c2a97938e4edfd
sheetalgcin <42903527+sheetalgcin@users.noreply.github.com>
2024-05-09 10:17:08 +0200

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,1 +1,10 @@
+import os
+
+# Access environment variables
+timestamp_seconds = os.environ.get('TIMESTAMP_SECONDS')
+iso8601_timestamp = os.environ.get('ISO8601_TIMESTAMP')
+
+# Print or use the values as needed
+print("Timestamp seconds:", timestamp_seconds)
+print("ISO8601 timestamp:", iso8601_timestamp)
 
