20fdd00f2cf2668889a4dccd771b322b7bceced2
sheetalgcin <42903527+sheetalgcin@users.noreply.github.com>
2024-05-09 10:21:52 +0200

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,8 +1,8 @@
 import os
 
 # Access environment variables
-timestamp_seconds = os.environ.get('TIMESTAMP_SECONDS')
-iso8601_timestamp = os.environ.get('ISO8601_TIMESTAMP')
+timestamp_seconds = os.environ.get('RELEASE_TAG')
+iso8601_timestamp = os.environ.get('RELEASE_DATE')
 
 # Print or use the values as needed
 print("Timestamp seconds:", timestamp_seconds)
