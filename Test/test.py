7a726b4e0c8a16e6157df01517dae6f85979cfa0
sheetalgcin <42903527+sheetalgcin@users.noreply.github.com>
2024-05-09 10:29:32 +0200

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -1,1 +1,11 @@
+import os
+
+# Access environment variables
+timestamp_seconds = os.environ.get('RELEASE_TAG')
+iso8601_timestamp = os.environ.get('RELEASE_DATE')
+
+# Print or use the values as needed
+print("Timestamp seconds:", timestamp_seconds)
+print("ISO8601 timestamp:", iso8601_timestamp)
+
 
