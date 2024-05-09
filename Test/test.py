b0451fa7896a2046a840ec8933fdd96bdf3346e5
sheetalgcin <42903527+sheetalgcin@users.noreply.github.com>
2024-05-09 12:06:55 +0200

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -1,6 +1,6 @@
 import os
 import datetime
-
+events = {"date": "", "component": "", "type": "", "tenant_id": "","version": ""}
 # Access environment variables
 release = os.environ.get('RELEASE_TAG')
 dateInfo = os.environ.get('RELEASE_DATE')
@@ -14,5 +14,13 @@ datetime_obj = datetime.datetime.fromisoformat(dateInfo)
 # Convert datetime object to Unix epoch time
 epoch_time = int(datetime_obj.timestamp())
 
+events["date"] = int(epoch_time)
+events["component"] = "zolagus"
+events["type"]  = "release"
+events["tenant_id"] = 1
+events["version"] = str(release)
+
+print(events)
+
 # Print the result
-print(epoch_time)
+ print(epoch_time)
